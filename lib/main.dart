4fb5e389ea977c2dc325f84d67b82fbc3a642f96
Javier Lopez <javierl.1994@gmail.com>
2020-10-04 15:13:01 -0600

    Add User Products Screen

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import './providers/products.dart';
 import './screens/cart_screen.dart';
 import './providers/orders.dart';
 import './screens/orders_screen.dart';
+import './screens/user_products_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -35,7 +36,8 @@ class MyApp extends StatelessWidget {
         routes: {
           ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
           CartScreen.routeName: (ctx) => CartScreen(),
-          OrdersScreen.routeName: (ctx) => OrdersScreen()
+          OrdersScreen.routeName: (ctx) => OrdersScreen(),
+          UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
         },
       ),
     );
